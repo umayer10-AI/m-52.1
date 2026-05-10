5567ac199c8556e1928ca8cb8135ea1c8ebc1ba0
umayer10-AI <mdumayer10@gmail.com>
2026-05-10 12:24:45 +0600

    add

--- wonderlust/src/app/destination/page.jsx
+++ wonderlust/src/app/destination/page.jsx
@@ -10,9 +10,9 @@ const page = async () => {
         <div>
             <h2 className='text-3xl font-semibold'>Explore All Destinations</h2>
             <h2 className='text-gray-400 mt-2'>Find your perfect travel experience from our curated collection</h2>
-            <div>
+            <div className='grid grid-cols-1 lg:grid-cols-4 gap-5'>
                 {
-                    data.map(v => <Carts key={v._id} p={data}></Carts>)
+                    data.map(v => <Carts key={v._id} p={v}></Carts>)
                 }
             </div>
         </div>
--- wonderlust/src/component/Carts.jsx
+++ wonderlust/src/component/Carts.jsx
@@ -1,23 +1,24 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Carts = ({p}) => {
     // console.log(p)
     return (
-        <div>
-            <div className="w-[260px] bg-white rounded-sm overflow-hidden shadow-md">
+        <div className='border shadow-lg shadow-cyan-600 rounded-xl p-4'>
+            <div className=" rounded-sm overflow-hidden shadow-md">
 
   {/* Image Section */}
   <div className="relative">
     <Image height={100} width={100}
-      src="https://i.ibb.co.com/5x7J0nS/beach.jpg"
-      
+      src={p.imageUrl}
+      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
       alt="Bali Paradise"
       className="w-full h-[170px] object-cover"
     />
 
     {/* Rating */}
-    <div className="absolute top-3 right-3 bg-white px-2 py-1 text-sm flex items-center gap-1 rounded-sm">
+    <div className="absolute top-3 right-3 bg-white/50 px-2 py-1 text-sm flex items-center gap-1 rounded-sm">
       <span>4.5</span>
       <span>★</span>
     </div>
@@ -28,17 +29,17 @@ const Carts = ({p}) => {
 
     {/* Country */}
     <p className="text-gray-500 text-sm flex items-center gap-1">
-      📍 Nepal
+      📍 {p.country}
     </p>
 
     {/* Title + Price */}
     <div className="flex justify-between items-start">
-      <h2 className="text-xl font-medium text-gray-800">
-        Bali Paradise
+      <h2 className="text-xl font-medium text-cyan-400">
+        {p.destinationName}
       </h2>
 
       <p className="text-2xl font-semibold text-black">
-        $2700
+        ${p.price}
         <span className="text-sm text-gray-400 font-normal">
           /Person
         </span>
@@ -47,13 +48,13 @@ const Carts = ({p}) => {
 
     {/* Duration */}
     <p className="text-gray-500 text-sm flex items-center gap-1">
-      📅 7 Days/6 Nights
+      📅 {p.duration}/6 Nights
     </p>
 
     {/* Button */}
-    <button className="mt-3 text-sky-500 text-sm font-medium flex items-center gap-2 hover:gap-3 transition-all">
+    <Link href={`/destination/${p._id}`} className="mt-3 text-sky-500 text-sm font-medium flex items-center gap-2 hover:gap-3 transition-all">
       BOOK NOW ↗
-    </button>
+    </Link>
 
   </div>
 </div>
