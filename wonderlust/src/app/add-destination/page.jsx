8643a1322e601db1c6ad42b44191fda7d1cfb2c6
umayer10-AI <mdumayer10@gmail.com>
2026-05-10 10:49:59 +0600

    add

--- wonderlust/src/app/add-destination/page.jsx
+++ wonderlust/src/app/add-destination/page.jsx
@@ -1,13 +1,20 @@
+"use client"
 import { Button, FieldError, Input, Label, ListBox, TextArea,Select, TextField } from '@heroui/react';
 import React from 'react';
 
-const page = () => {
+const Add = () => {
 
-    
+  const a = async (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const newUser = Object.fromEntries(formData.entries())
+    console.log(newUser)
+  }
+
 
     return (
         <div className="w-1/2 mx-auto bg-slate-900 border border-slate-800 rounded-3xl shadow-2xl">
-  <form className="p-10 space-y-8">
+  <form onSubmit={a} className="p-10 space-y-8">
 
     <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
 
@@ -96,7 +103,7 @@ const page = () => {
 
         <Input
           type="number"
-          placeholder="1299"
+          placeholder="1299" 
           className="rounded-2xl bg-slate-800 text-white border border-slate-700 placeholder:text-slate-400"
         />
 
@@ -110,7 +117,7 @@ const page = () => {
         </Label>
 
         <Input
-          placeholder="7 Days / 6 Nights"
+          placeholder="7 Days / 6 Nights" 
           className="rounded-2xl bg-slate-800 text-white border border-slate-700 placeholder:text-slate-400"
         />
 
@@ -157,7 +164,7 @@ const page = () => {
             Description
           </Label>
 
-          <TextArea
+          <TextArea 
             placeholder="Describe the travel experience..."
             className="rounded-3xl bg-slate-800 text-white border border-slate-700 placeholder:text-slate-400"
           />
@@ -179,4 +186,4 @@ const page = () => {
     );
 };
 
-export default page;
+export default Add;
