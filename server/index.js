d76af99e547fa014f58f3d7cd4b40b77ff40ce36
umayer10-AI <mdumayer10@gmail.com>
2026-05-10 11:01:34 +0600

    add

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000
+dotenv.config()
+
+app.use(cors())
+app.use(express.json())
+
+const uri = process.env.MY
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const run = async () => {
+    try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World Umayer')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
