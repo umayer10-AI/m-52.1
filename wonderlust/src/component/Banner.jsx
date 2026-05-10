625adb17d37976ba89c05727f4c378beb0b468f2
umayer10-AI <mdumayer10@gmail.com>
2026-05-10 10:21:12 +0600

    add

--- wonderlust/src/component/Banner.jsx
+++ wonderlust/src/component/Banner.jsx
@@ -2,7 +2,7 @@ import { Separator } from "@heroui/react";
 
 const Banner = () => {
   return (
-    <div className="bg-[url('/assets/banner.png')] text-white  flex justify-between flex-col items-center  gap-5 h-150">
+    <div className="bg-[url('/assets/banner.png')] text-white rounded-xl overflow-hidden flex justify-between flex-col items-center gap-5 h-[60vh]">
       <div className="p-10 text-center flex justify-center flex-col items-center gap-3.5 flex-1">
         <h1 className="text-7xl">
           Discover Your <br /> Next Adventure
