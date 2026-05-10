be5b77c927165b94fe0af345f974ef8c3c9cd420
umayer10-AI <mdumayer10@gmail.com>
2026-05-10 12:07:42 +0600

    add

--- wonderlust/src/app/destination/page.jsx
+++ wonderlust/src/app/destination/page.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            Hello destination
+            <h2 className='text-3xl font-semibold'>Explore All Destinations</h2>
+            <h2 className='text-gray-400'>Find your perfect travel experience from our curated collection</h2>
         </div>
     );
 };
