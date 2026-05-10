91d864eb9f811305580d54da55b47ec551a34066
umayer10-AI <mdumayer10@gmail.com>
2026-05-10 22:37:51 +0600

    add

--- wonderlust/src/app/booking/page.jsx
+++ wonderlust/src/app/booking/page.jsx
@@ -7,8 +7,8 @@ const page = () => {
     return (
         <div>
             <div className='space-y-2 my-10 '>
-                <h2 className='text-3xl font-bold text-center'>My Booking</h2>
-                <h2 className='text-gray-400 text-center'>Manage and view your upcoming travel plans</h2>
+                <h2 className='text-3xl font-bold'>My Booking</h2>
+                <h2 className='text-gray-400'>Manage and view your upcoming travel plans</h2>
             </div>
             <Booking />
         </div>
